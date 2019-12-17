1dd7d065a8c452879f628fb8e338a5f6ff79160e
root <root@ip-10-1-1-100.ec2.internal>
2019-12-17 06:40:46 +0000

    added comments in the file

--- file2.sh
+++ file2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#adding to git 
+#addding to git
 echo "iam going to create a shell script which can take input of a website and ping it for 5 times"
 read -p "please enter the website (eg. www.google.com)" website
 ping -c 5 $website
